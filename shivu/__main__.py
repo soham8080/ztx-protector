85a33ed11b676b79e174f39c05ef99c2520f7ac6
ishu9805 <abhishek47903@gmail.com>
2024-04-14 16:26:16 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -14,7 +14,7 @@ from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filter
 from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu
 from shivu import application, SUPPORT_CHAT, UPDATE_CHAT, db, LOGGER
 from shivu.modules import ALL_MODULES
-from shivu.modules.coin import add_coins
+
 
 locks = {}
 message_counters = {}
