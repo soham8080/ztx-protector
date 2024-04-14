ba04ca008b6b05d59bea7b65fc6269d8e939e1b3
ishu9805 <abhishek47903@gmail.com>
2024-04-14 14:42:50 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -10,7 +10,7 @@ from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters
 
-from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu
+from shivu import shivuu
 from shivu import application, SUPPORT_CHAT, UPDATE_CHAT, db, LOGGER
 from shivu.modules import ALL_MODULES
 
