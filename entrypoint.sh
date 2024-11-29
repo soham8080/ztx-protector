411ed66533bf4d8eff05f54a3bc4d2a4cda57a37
soham8080 <sagarshinde902111@gmail.com>
2024-11-29 21:03:53 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
+# Run both Python files in the background
 python3 app.py &  # Running app.py in the background
-python3 -m shivu # Running shivu module
+python3 -m shivu  # Running shivu module
